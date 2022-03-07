2d39cc976c4a23c1ed83966f2cf7eaf306301cc6
Edno Almeida <edno2819@hotmail.com>
2022-03-07 12:06:56 -0300

    .

--- catalogador/buscas/screduler.py
+++ catalogador/buscas/screduler.py
@@ -12,7 +12,7 @@ def extracts():
     now = datetime.today() + timedelta(seconds=120)
     tomorow = now + timedelta(days=1)
     scheduler = BackgroundScheduler(timezone=BR)
-    #scheduler.add_job(cron.ResetValues, 'cron', hour=now.hour, minute=now.minute, day=now.day, year=now.year, end_date=tomorow.__str__()[:10])
+    scheduler.add_job(cron.ResetValues, 'cron', hour=now.hour, minute=now.minute, day=now.day, year=now.year, end_date=tomorow.__str__()[:10])
     scheduler.add_job(cron.extract_1_1_Django, 'cron', day_of_week='1,2,3,4,5', hour=14, minute=0)
     scheduler.add_job(cron.all_dairly_task, CronTrigger.from_crontab('1 0 * * 2,3,4,5,6'))
     scheduler.start()
