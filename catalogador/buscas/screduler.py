72777dd984373dcb876c48a2e11b32516f971488
Edno Almeida <edno2819@hotmail.com>
2022-03-09 11:28:41 -0300

    .

--- catalogador/buscas/screduler.py
+++ catalogador/buscas/screduler.py
@@ -1,5 +1,4 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
 from datetime import datetime, timedelta
 import backports.zoneinfo as zoneinfo
 from buscas import cron
@@ -14,7 +13,7 @@ def extracts():
 
     now = datetime.today() + timedelta(seconds=120)
     tomorow = now + timedelta(days=1)
-    #scheduler.add_job(cron.ResetValues, 'cron', hour=now.hour, minute=now.minute, day=now.day, year=now.year, end_date=tomorow.__str__()[:10]+' 00:00:00.000000')
+    scheduler.add_job(cron.ResetValues, 'cron', hour=now.hour, minute=now.minute, day=now.day, year=now.year, end_date=tomorow.__str__()[:10]+' 00:00:00.000000')
 
     scheduler.add_job(cron.extract_1_1_Django, 'cron', day_of_week='0,1,2,3,4', hour=14, minute=0)
     scheduler.add_job(cron.all_dairly_task, 'cron', day_of_week='1,2,3,4,5', hour=0, minute=1)
--- catalogador/buscas/views.py
+++ catalogador/buscas/views.py
@@ -38,24 +38,41 @@ def busca(request):
 
     if request.method == "POST":
         par = request.POST.get('par', False)
+
         time = int(request.POST.get('time', False))
+
         hora = request.POST.get('hora', False)
         hora = int(hora) if hora!='' else False
 
         minuto = request.POST.get('minuto', False)
         minuto = int(minuto) if minuto!='' else False
 
+        main_taxa = request.POST.get('main_taxa', False)
+        main_taxa = int(main_taxa) if main_taxa!='' else False
+
+        second_taxa = request.POST.get('second_taxa', False)
+        second_taxa = int(second_taxa) if second_taxa!='' else False
+
 
 
         datas = Chance.objects.filter(par=par) if par else Chance.objects.all() 
         datas = datas.filter(hora=hora) if hora else datas
         datas = datas.filter(minuto=minuto) if minuto else datas
+        datas = datas.filter(porcent=main_taxa) if main_taxa else datas
         datas = datas.filter(timeframe=time) if time else datas
         datas = datas.order_by('-porcent','-par')
 
         chances = format_chance(datas)
 
-        context.update({'chances': chances, 'resultado_qtd':len(datas)})
+        if type(second_taxa)==int:
+            new_chance = []
+            for chance in chances:
+                if 'porcent_before' in chance.keys() and chance['porcent_before']>=second_taxa:
+                    new_chance.append(chance)
+        else:
+            new_chance = chances
+
+        context.update({'chances': new_chance, 'resultado_qtd':len(new_chance)})
         return render(request, 'result.html', context) 
 
     return render(request, 'result.html', context) 
